b70d17f415da00f08120a364fe26773b49f4920d
Dannyjoe07 <dannyjoeanto@gmail.com>
2023-06-19 21:02:02 +0530

    EC2 - Update

---
+++ vars.tf
@@ -0,0 +1,22 @@
+variable "instance_name" {
+  default = "default-instance"
+}
+variable "instance_count" {
+  default = "1"
+}
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "ami_id" {}
+variable "subnet_id" {}
+variable "role_name" {}
+variable "policy_arn" {
+  default = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+variable "policy_arn2" {
+  default = "arn:aws:iam::aws:policy/AmazonSSMPatchAssociation"
+}
+variable "profile_name" {
+  default = "SSM_Role"
+}
+
